29cbbb9c4bb5530adeb1e150f1d44aff3f6d103a
ArlanchikDrey <arlanchikdrey@yandex.ru>
2020-04-07 20:08:00 +0300

    added new module - data

---
+++ lib/repository/models/User.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class User {
+  User({
+    @required this.name,
+    @required this.surname,
+  });
+
+  final String name;
+  final String surname;
+}
