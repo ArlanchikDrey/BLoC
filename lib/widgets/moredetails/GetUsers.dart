2e21ededaefbc83235e62c2ed01ad41dc5a3bca5
ArlanchikDrey <arlanchikdrey@yandex.ru>
2020-04-17 10:54:11 +0300

    add animation Flutter Logo in DetailPage

--- lib/widgets/moredetails/GetUsers.dart
+++ lib/widgets/moredetails/GetUsers.dart
@@ -7,9 +7,20 @@ class GetUsers extends StatefulWidget{
   }
 }
 
-class UserState extends State<GetUsers>{
+class UserState extends State<GetUsers> with TickerProviderStateMixin{
+  AnimationController _controller;
+  CurvedAnimation _curve;
+
   String _response = "default";
   int _i = 0;
+
+  @override
+  void initState() {
+    _controller = AnimationController(duration: const Duration(milliseconds: 2200), vsync: this);
+    _curve = CurvedAnimation(parent: _controller, curve: Curves.easeInCirc);
+    _controller.forward();
+    super.initState();
+  }
 
   void _updateText(){
     setState(() {
@@ -19,10 +30,12 @@ class UserState extends State<GetUsers>{
 
   @override
   Widget build(BuildContext context) {
-    
     return Center(
       child: Column(
         children: [
+          FadeTransition(
+            opacity: _curve,
+            child: FlutterLogo(size: 150.0)),
           Text(_response),
           FloatingActionButton(
             onPressed: _updateText,
