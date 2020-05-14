d73ea7486e0b316dd662fc9e57267a966bd07dd0
ArlanchikDrey <arlanchikdrey@yandex.ru>
2020-05-14 11:59:48 +0300

    deleted old rep

--- lib/widgets/timer_module/ticker.dart
+++
@@ -1,9 +0,0 @@
-//All our Ticker class does is provide a tick function that takes the desired number 
-//of ticks (seconds) 
-//and returns a stream that generates the remaining seconds every second.
-class Ticker {
-  Stream<int> tick({int ticks}) {
-    return Stream.periodic(Duration(seconds: 1), (x) => ticks - x - 1)
-        .take(ticks);
-  }
-}
